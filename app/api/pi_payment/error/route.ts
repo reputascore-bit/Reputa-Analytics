143de0b4d11def8fc8eccce56b91b79e73206f5c
Mediceberg <elmehdi.meliani1@gmail.com>
2026-01-16 20:42:28 +0100

    Update route.ts

--- app/api/pi_payment/error/route.ts
+++ app/api/pi_payment/error/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server'; 
 import { errorPOST } from 'pi-sdk-nextjs';
 
 export async function POST(req: NextRequest) {

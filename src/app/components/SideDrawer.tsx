d8d31e12f7b04fc826d6e6eea7da9f06f5400db0
Mediceberg <elmehdi.meliani1@gmail.com>
2026-01-31 19:21:38 +0100

    Update SideDrawer.tsx

--- src/app/components/SideDrawer.tsx
+++ src/app/components/SideDrawer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'; 
+import { useState, useEffect } from 'react';  
 import { 
   X, Home, LineChart, Activity, User, Globe, Wallet, 
   FileText, Settings, MessageSquare, HelpCircle, Shield, 
